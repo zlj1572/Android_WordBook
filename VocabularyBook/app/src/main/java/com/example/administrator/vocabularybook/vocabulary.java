2cbe57892e25408348250eff287096854c477d9b
Blanc <zlj5676@gmail.com>
2018-04-07 13:41:36 +0800

    vocabularybook1

---
+++ VocabularyBook/app/src/main/java/com/example/administrator/vocabularybook/vocabulary.java
@@ -0,0 +1,13 @@
+package com.example.administrator.vocabularybook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vocabulary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vocabulary);
+    }
+}
